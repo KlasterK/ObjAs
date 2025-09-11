aaf326ae26ebe56962890724031cfbc71ac32038
KlasterK <kadakinvi2@gmail.com>
2025-09-11 13:56:23 +0000

    Add support of transforming AST to CLI

--- src/__main__.py
+++ src/__main__.py
@@ -3,20 +3,29 @@ import sys
 from lark import Lark
 import argparse
 import logging
+
+from .transformer import ObjAsTransformer
 from .grammar import grammar
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('file', action='store', nargs='+')
-    parser.add_argument('--ast-only', action='store_true')
+    parser.add_argument('--ast-only', action='store_true', help='Print AST and exit')
+    parser.add_argument(
+        '--no-transform',
+        action='store_true',
+        help='Do not transform AST (use only with --ast-only)',
+    )
     args = parser.parse_args()
 
     if not args.ast_only:
         logging.critical('ObjAs only supports printing of AST right now')
         sys.exit(1)
 
-    parser = Lark(grammar, parser='lalr')
+    parser = Lark(
+        grammar, parser='lalr', transformer=None if args.no_transform else ObjAsTransformer()
+    )
 
     for file_name in args.file:
         if file_name == '-':
